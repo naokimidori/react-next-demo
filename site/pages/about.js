e53b8bea69bf1db5e1d0be968470fdaf1b1104e0
zhaolong <naokimidori@163.com>
2021-04-16 22:37:55 +0800

    fix

--- site/pages/about.js
+++ site/pages/about.js
@@ -10,7 +10,7 @@ export default function About() {
   return (
     <div>
       <h1>about_page</h1>
-      <Link href='/test?name=bob'>
+      <Link href='/test?name=vino'>
         <a>to test  </a>
       </Link>
     </div>
